610387ba5bb0cc5e75d75df2fdb67ca5f3942a18
DSMPhillips <129297874+DSMPhillips@users.noreply.github.com>
2023-10-25 15:44:25 -0400

    Update menu-button-plugin.js

--- menu-button-plugin.js
+++ menu-button-plugin.js
@@ -1,5 +1,32 @@
 videojs.registerPlugin('pluginName', function() {
-    var myPlayer = this;
-    // Complete JavaScript
-    // ...
+    //var myPlayer = this;
+    <script src="//path/to/video.js"></script>
+    <script src="//path/to/videojs-settings-menu.js"></script>
+    <script>
+	var player = videojs('videojs-settings-menu-player', {
+		'playbackRates': [0.5, 1, 1.5, 2],
+		controlBar: {
+			children: {
+				'playToggle':{},
+				'muteToggle':{},
+				'volumeControl':{},
+				'currentTimeDisplay':{},
+				'timeDivider':{},
+				'durationDisplay':{},
+				'liveDisplay':{},
+
+				'flexibleWidthSpacer':{},
+				'progressControl':{},
+
+				'settingsMenuButton': {
+					entries : [
+						'subtitlesButton',
+						'playbackRateMenuButton'
+					]
+				},
+				'fullscreenToggle':{}
+			}
+		}
+	});
+</script>
   });
